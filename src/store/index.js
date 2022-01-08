1e58493502dcec4cff482759bc22de9511a885d0
Kovalenko23 <vlad.kovalenko.od@gmail.com>
2022-01-08 20:26:59 +0200

    first test

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,64 @@
 import { createStore } from 'vuex'
+import axios from 'axios';
+
 
 export default createStore({
   state: {
+    catsJson: null,
+    tags: null,
   },
+
+  getters: {
+    getJson: state => {
+      return state.catsJson;
+    },
+    getTags: state => {
+      return state.tags;
+
+    }
+  
+  },
+
   mutations: {
+    SET_JSON(state, payload) {
+      state.catsJson = payload;
+    },
+    SET_TAGS(state, payload) {
+      const tagsArr = []
+
+      for (let i of payload) {
+        let arr = i.tags.split(',')
+        for (let k of arr) {
+          if (!tagsArr.includes(k.trim())) {
+            tagsArr.push(k.trim())
+          }
+        }
+      }
+      state.tags = tagsArr;
+    } 
   },
   actions: {
+
+    async loadJson({ commit }, value) {
+      const errors = [];
+      await axios
+        .get(`https://pixabay.com/api/?key=25040045-450d2ba4de6c5fa6527432375&q=cats&image_type=all&per_page=100`)
+        .then(response => {
+
+          
+          commit(`SET_TAGS`, response.data.hits)
+
+
+          if (value === `Likes`) commit(`SET_JSON`, response.data.hits.sort((a, b) => b.likes - a.likes));
+          else if (value === `Comments`) commit(`SET_JSON`, response.data.hits.sort((a, b) => b.comments - a.comments));
+          else commit(`SET_JSON`, response.data.hits)
+        })
+        .catch(e => {
+          errors.push(e);
+        })
+      if (errors.length !== 0) console.log(errors);
+    },
+
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,78 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<label class="sort-label" for="sort">sort</label>
+<select v-model="selected" @change="sortJson" name="option" class="sort" id="sort">
+    <option class="sort__item" value="">-</option>
+    <option class="sort__item"  value="Likes">Likes</option>
+    <option class="sort__item" value="Comments">Comments</option>
+</select>
+<div class="filter">
+    <input @keyup="debouncedHandler"  type="text" placeholder="filter" class="filter__tags">
+</div>
+
+<div  class="container">
+  <div class="card"  v-for="card in cards" :key="card.id">
+        <a v-bind:href="card.pageURL" target="_blank" class="card__image">
+            <img v-bind:src="card.largeImageURL" alt="random-picture">
+        </a>
+        <div class="card__social">
+            <div class="card__social_likes"><i class="likes fas fa-thumbs-up"></i> {{ card.likes}}</div>
+            <div class="card__social_comments">{{ card.comments}}<i class="comments fas fa-comments"></i></div>
+        </div>
+        <div class="card__tags">
+            <span v-for="tag of this.tagsList(card)" :key="tag" class="card__tags-item">{{ tag }}</span>
+
+
+    </div>
   </div>
+</div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+<script>
+import {debounce} from 'lodash'
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+    
+    name: 'CatsList',
+        data() {
+            return {
+                selected: '',
+                inputField: '',
+            }
+        },
+    computed: {
+        cards() {
+            let newArray = [];
+            if (this.inputField === '') return this.$store.getters.getJson; 
+            else {
+                for (let i of this.$store.getters.getJson) {
+                    if (i.tags.split(/,| /).find(word => word.trim() === this.inputField)) newArray.push(i)
+                };
+                return newArray
+            }
+        },
+        tags() {
+            return this.$store.getters.getTags;
+        },
+        debouncedHandler() {
+            return debounce(this.handler, 300)
+        }
+    },
+    methods: {
+        handler (event) {
+            let value = event.target.value;
+            console.log(value);
+            this.inputField = value;
+        },
+        tagsList(arg) {
+            let cardTags = [];
+            for (let i of arg.tags.split(',')) {
+                cardTags.push(i.trim());
+            };
+            return cardTags
+        },
+        sortJson() {
+            this.$store.dispatch(`loadJson`, this.selected)
+        }
+    },
 }
 </script>
